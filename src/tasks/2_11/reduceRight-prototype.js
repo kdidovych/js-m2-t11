47b54aa5c0638d22ac296aeb0df7c1ca32c99266
Kostiantyn Didovych <didovych.k.v@gmail.com>
2026-04-23 15:12:47 +0300

    2.11 reduceRight prototype [finalized: remove @todo-es, added arguments

--- src/tasks/2_11/reduceRight-prototype.js
+++ src/tasks/2_11/reduceRight-prototype.js
@@ -1,41 +1,44 @@
 'use strict';
 
-const myArray = [[0, 1], 1, 'one', { a: 'b', c: 'd' }, '', {}, , NaN, undefined, null, Infinity, 0, []];
-console.log(myArray.reduceRight((a, b) => a.concat(b), []));
+// unable to implement 'empty'. Using 'undefined' instead for now
+function MyArray() {
+    if (arguments.length === 1 && Number.isInteger(arguments[0]) && arguments[0] >= 0 && arguments[0] < (2 ** 32 - 1)) {
+        this.length = arguments[0];
+    } else {
+        this.length = arguments.length;
+        for (let i = 0; i < arguments.length; i++) {
+            this[i] = arguments[i];
+        }
+    }
+}
 const myReduceRight = function (callback, initialValue) {
-    let result = initialValue !== undefined ? initialValue : this[this.length - 1];
-    const initialIndex = this.length - 1 - (initialValue !== undefined);
+    let lastNonEmptyIndex = -1;
+    for (let i = this.length - 1; i >= 0; i--) {
+        if (typeof this[i] !== 'undefined') {
+            lastNonEmptyIndex = i;
+            break;
+        }
+    }
+    if (lastNonEmptyIndex < 0 && initialValue === undefined) {
+        throw new TypeError('Reduce of empty array with no initial value');
+    }
+    let result = initialValue ?? this[lastNonEmptyIndex];
+    const initialIndex = lastNonEmptyIndex - (initialValue === undefined);
     if (initialIndex < 0) return result;
     for (let i = initialIndex; i >= 0; i--) {
-        result = callback(result, this[i], i, this);
+        if (this[i] !== undefined) {
+            result = callback(result, this[i], i, this);
+        }
     }
-    console.log('myReduceRight'); // @todo discuss
     return result;
-};
-console.log(myArray);
+}
+MyArray.prototype.reduceRight = myReduceRight;
 
-// MyArray constructor. Object.create() @todo discuss
-function MyArray(...inputArray) {
-    this.array = inputArray;
-    //this.reduceRight = [].reduceRight.bind(this.array); // @todo discuss
-};
-MyArray.prototype.reduceRight = function (callback, initialValue) {
-    let result = initialValue !== undefined ? initialValue : this[this.array.length - 1];
-    const initialIndex = this.array.length - 1 - (initialValue !== undefined);
-    if (initialIndex < 0) return result;
-    for (let i = initialIndex; i >= 0; i--) {
-        result = callback(result, this.array[i], i, this.array);
-    }
-    console.log('prototype myReduceRight'); // @todo discuss
-    return result;
-};
-const myArray1 = new MyArray(...myArray);
-console.log(myArray1.reduceRight((a, b) => a.concat(b), []));
-
-// __proto__
-const myArray2 = Object.create(myArray); // no Array.create() @todo discuss
-myArray2.__proto__.reduceRight = myReduceRight;
-console.log(myArray2.reduceRight((a, b) => a.concat(b), []));
-
-// @todo discuss why initial array was affected while Object.create was used
-console.log(myArray.reduceRight((a, b) => a.concat(b), []));
+const testFunc = (a, b) => a - b;
+// console.dir(new MyArray().reduceRight(testFunc); // TypeError
+console.dir(new MyArray(-2).reduceRight(testFunc));
+// console.dir(new MyArray(10).reduceRight(testFunc); // TypeError
+console.dir(new MyArray('10').reduceRight(testFunc));
+console.dir(new MyArray(null, { a: 5 }, [2, 4, 5], '', undefined).reduceRight(testFunc));
+console.dir(new MyArray(1, 2, 3, 4, 5).reduceRight(testFunc));
+console.dir(new MyArray(1, 2, 3, 4, 5).reduceRight(testFunc, -100));
